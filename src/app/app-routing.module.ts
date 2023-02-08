e71d24b317da252709bcd51f3dde55e621116ba8
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-02-08 10:11:37 +0100

    enlaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'empleado', component: EmpleadoComponent},
+  {path: '', component: HomeComponent},
+  {path: '**', component: EmpleadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpleadoComponent
+    EmpleadoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
