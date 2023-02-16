523bb7dbabe5f0402cf761bd5c44459dc1192f53
Marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-02-16 09:22:29 +0100

    prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EmpleadoComponent } from './empleado/empleado.component';
 import { HomeComponent } from './home/home.component';
-import { PatineteComponent } from './patinete/patinete.component';
 import { PrincipalComponent } from './principal/principal.component';
 
 
 const routes: Routes = [
-  {path: 'empleado', component: EmpleadoComponent},
   //se pueden pasar parámetros a traves de la url con la sintaxis:
   //{path: 'coche/:id'...}
   //En el constructor del componente debo declarar el id + usar actRoute
-  {path: 'patinete', component: PatineteComponent},
   {path: '', component: HomeComponent},
   {path: 'principal', component: PrincipalComponent},
-  {path: '**', component: EmpleadoComponent}
+  {path: '**', component: HomeComponent}
 ];
 
 @NgModule({
